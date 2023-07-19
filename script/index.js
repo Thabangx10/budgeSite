c1156f615602c88596dd82eb2863a3d74d443715
Thabangx10 <Thabangx12@gmail.com>
2023-07-19 10:19:06 +0200

    updates, will provide more scripting

--- script/index.js
+++ script/index.js
@@ -15,6 +15,42 @@ let totalExpenses = 0;
 let expenses = [];
 let incomes = [];
 
+// LANGUAGE TRANSCRIPTS
+const transcripts = {
+  en: {
+    title: "English Transcript",
+    content: "This is the English transcript of the video.",
+  },
+  xh: {
+    title: "Xhosa Transcript",
+    content: "Lomfuduzo unokufunda ku-Xhosa.",
+  },
+  af: {
+    title: "Afrikaans Transcript",
+    content: "Hierdie is die Afrikaanse transkripsie van die video.",
+  },
+};
+
+function changeLanguage(language) {
+  // Hide all language transcripts
+  const allTranscripts = document.querySelectorAll(".transcript");
+  allTranscripts.forEach((transcript) => {
+    transcript.style.display = "none";
+  });
+
+  // Show the selected language transcript
+  const selectedTranscript = document.querySelector(`.lang-${language}`);
+  selectedTranscript.style.display = "block";
+
+  // Update the language title
+  const pageTitle = document.getElementById("page-title");
+  pageTitle.textContent = `PEAK! - ${language.toUpperCase()}`;
+
+  // Update the language visualization based on the selected language
+  updateExpenseVisualization();
+  updateIncomeVisualization();
+}
+
 const expenseVisualizationList = document.getElementById(
   "expenseVisualizationList"
 );
@@ -22,10 +58,86 @@ const incomeVisualizationList = document.getElementById(
   "incomeVisualizationList"
 );
 
+function createCombinedPieChart(incomeLabels, incomeData, expenseLabels, expenseData, chartId) {
+  const chartContainer = document.getElementById(chartId);
+  const existingChart = Chart.getChart(chartContainer); // Check if a Chart instance already exists
+
+  if (existingChart) {
+    existingChart.destroy(); // Destroy the existing Chart instance
+  }
+
+  const ctx = chartContainer.getContext("2d");
+
+  new Chart(ctx, {
+    type: "pie",
+    data: {
+      labels: [...incomeLabels, ...expenseLabels],
+      datasets: [
+        {
+          data: [...incomeData, ...expenseData],
+          backgroundColor: [
+            "#FF6384",
+            "#36A2EB",
+            "#FFCE56",
+            "#008080",
+            "#800080",
+            "#FF4500",
+            "#FF8C00",
+            "#8A2BE2",
+            "#7CFC00",
+            "#4B0082",
+            "#FF1493",
+            "#00FF00",
+            "#00BFFF",
+          ],
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      legend: {
+        display: true,
+        position: "right",
+      },
+      tooltips: {
+        callbacks: {
+          label: function (tooltipItem, data) {
+            const dataset = data.datasets[tooltipItem.datasetIndex];
+            const total = dataset.data.reduce((previousValue, currentValue) => previousValue + currentValue);
+            const currentValue = dataset.data[tooltipItem.index];
+            const percentage = Math.floor(((currentValue / total) * 100) + 0.5);
+            return `${data.labels[tooltipItem.index]}: ${currentValue} ( ${percentage}% )`;
+          },
+        },
+      },
+    },
+  });
+}
+
+// Update the income and expense charts
+function updateCombinedChart() {
+  const incomeLabels = incomes.map((income) => income.source);
+  const incomeAmounts = incomes.map((income) => income.amount.toFixed(2));
+  const expenseLabels = expenses.map((expense) => expense.name);
+  const expenseAmounts = expenses.map((expense) => expense.amount.toFixed(2));
+
+  createCombinedPieChart(incomeLabels, incomeAmounts, expenseLabels, expenseAmounts, "combinedChart");
+}
+
+function updateCharts() {
+  updateCombinedChart();
+}
+
+
 function updateExpenseVisualization() {
+  const expenseVisualizationList = document.getElementById(
+    "expenseVisualizationList"
+  );
   expenseVisualizationList.innerHTML = "";
 
-  expenses.forEach((expense) => {
+  expenses.forEach((expense, index) => {
     const expenseItem = document.createElement("li");
     expenseItem.textContent = `${expense.name}: R${expense.amount.toFixed(2)}`;
     expenseVisualizationList.appendChild(expenseItem);
@@ -33,13 +145,37 @@ function updateExpenseVisualization() {
 }
 
 function updateIncomeVisualization() {
+  const incomeVisualizationList = document.getElementById(
+    "incomeVisualizationList"
+  );
   incomeVisualizationList.innerHTML = "";
 
-  incomes.forEach((income) => {
+  incomes.forEach((income, index) => {
     const incomeItem = document.createElement("li");
     incomeItem.textContent = `${income.source}: R${income.amount.toFixed(2)}`;
     incomeVisualizationList.appendChild(incomeItem);
   });
+}
+
+// Create the expense chart
+function updateExpenseChart() {
+  const expenseLabels = expenses.map((expense) => expense.name);
+  const expenseAmounts = expenses.map((expense) => expense.amount.toFixed(2));
+
+  createPieChart(expenseLabels, expenseAmounts, "expenseChart");
+}
+
+// Update the income chart
+function updateIncomeChart() {
+  const incomeLabels = incomes.map((income) => income.source);
+  const incomeAmounts = incomes.map((income) => income.amount.toFixed(2));
+
+  createPieChart(incomeLabels, incomeAmounts, "incomeChart");
+}
+
+function updateCharts() {
+  updateExpenseChart();
+  updateIncomeChart();
 }
 
 function calculateIncomeOverExpense() {
@@ -87,6 +223,8 @@ function loadFromLocalStorage() {
 // Load data from Local Storage when the page is loaded
 window.addEventListener("load", () => {
   loadFromLocalStorage();
+  updateExpenseVisualization();
+  updateIncomeVisualization();
 });
 
 function updateExpenses() {
@@ -98,17 +236,17 @@ function updateExpenses() {
     const expenseItem = document.createElement("div");
     expenseItem.classList.add("expense-item");
     expenseItem.innerHTML = `
-            <span>${expense.name}</span>
-            <span>R${expense.amount.toFixed(2)}</span>
-            <button onclick="removeExpense(${index})">Remove</button>
-        `;
+      <span>${expense.name}</span>
+      <span>R${expense.amount.toFixed(2)}</span>
+      <button onclick="removeExpense(${index})">Remove</button>
+    `;
     expenseList.appendChild(expenseItem);
   });
 
   calculateIncomeOverExpense();
-
   updateExpenseVisualization();
   updateLocalStorage();
+  updateCharts();
 }
 
 function addExpense(e) {
@@ -123,8 +261,6 @@ function addExpense(e) {
     expenseNameInput.value = "";
     expenseAmountInput.value = "";
   }
-  updateExpenseVisualization();
-  updateLocalStorage();
 }
 
 function removeExpense(index) {
@@ -132,8 +268,6 @@ function removeExpense(index) {
     expenses.splice(index, 1);
     updateExpenses();
   }
-  updateExpenseVisualization();
-  updateLocalStorage();
 }
 
 function updateIncome() {
@@ -145,17 +279,17 @@ function updateIncome() {
     const incomeItem = document.createElement("div");
     incomeItem.classList.add("income-item");
     incomeItem.innerHTML = `
-            <span>${income.source}</span>
-            <span>R${income.amount.toFixed(2)}</span>
-            <button onclick="removeIncome(${index})">Remove</button>
-        `;
+      <span>${income.source}</span>
+      <span>R${income.amount.toFixed(2)}</span>
+      <button onclick="removeIncome(${index})">Remove</button>
+    `;
     incomeList.appendChild(incomeItem);
   });
 
   calculateIncomeOverExpense();
-
-  updateExpenseVisualization();
+  updateIncomeVisualization();
   updateLocalStorage();
+  updateCharts();
 }
 
 function addIncome(e) {
@@ -170,9 +304,6 @@ function addIncome(e) {
     incomeSourceInput.value = "";
     incomeAmountInput.value = "";
   }
-
-  updateExpenseVisualization();
-  updateLocalStorage();
 }
 
 function removeIncome(index) {
@@ -180,17 +311,13 @@ function removeIncome(index) {
     incomes.splice(index, 1);
     updateIncome();
   }
-
-  updateExpenseVisualization();
-  updateLocalStorage();
 }
 
 // Event listeners for form submissions
 expenseForm.addEventListener("submit", addExpense);
 incomeForm.addEventListener("submit", addIncome);
 
-// Load data from Local Storage when the page is loaded
-window.addEventListener("load", () => {
+document.addEventListener("DOMContentLoaded", () => {
   loadFromLocalStorage();
   updateExpenseVisualization();
   updateIncomeVisualization();
